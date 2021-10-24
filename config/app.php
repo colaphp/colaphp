085050e9d01119fa6dbfc2248c651a413b78a931
wanganlin <inbox@wanganlin.com>
2021-10-24 20:16:58 +0800

    更新providers

--- config/app.php
+++ config/app.php
@@ -4,4 +4,13 @@ return [
     'debug' => env('APP_DEBUG', false),
     'default_timezone' => 'UTC', // or Asia/Shanghai
     'default_themes' => 'default',
+
+    'providers' => [
+        \Swift\Container\ContainerProvider::class,
+        \Swift\Session\SessionProvider::class,
+        \Swift\Database\LaravelProvider::class,
+        \Swift\Redis\RedisProvider::class,
+        \Swift\Log\LogProvider::class,
+        \Swift\Translation\TranslationProvider::class,
+    ]
 ];
