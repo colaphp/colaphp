057e1ee4e85618b3fa0c453696bb10c176532e20
wanganlin <inbox@wanganlin.com>
2021-10-26 15:23:31 +0800

    增加日志文件

--- config/server.php
+++ config/server.php
@@ -9,7 +9,8 @@ return [
     'user' => env('SERVER_PROCESS_USER', ''),
     'group' => env('SERVER_PROCESS_GROUP', ''),
     'pid_file' => runtime_path() . '/app.pid',
-    'max_request' => 1000000,
     'stdout_file' => runtime_path() . '/logs/stdout.log',
+    'log_file' => runtime_path() . '/logs/app.log',
+    'max_request' => 1000000,
     'max_package_size' => 10 * 1024 * 1024
 ];
