3c6e16cc641926b1bf2e808b0113822db37a8847
wanganlin <inbox@wanganlin.com>
2021-12-30 15:50:00 +0800

    增加command

---
+++ app/Command/HelloCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class HelloCommand
+ * @package App\Command
+ */
+class HelloCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'hello';
+
+    /**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
