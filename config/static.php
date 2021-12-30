ab7c6bd6a4ded77b9a04aaa2829c69eb3c2ce25a
wanganlin <inbox@wanganlin.com>
2021-12-30 11:14:26 +0800

    wip

--- config/static.php
+++ config/static.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'enable' => true, // 是否支持静态文件
-    'middleware' => [ // 静态文件中间件
+    'enable' => true,
+    'middleware' => [ // Static file Middleware
         \App\Http\Middleware\StaticFile::class,
     ],
 ];
