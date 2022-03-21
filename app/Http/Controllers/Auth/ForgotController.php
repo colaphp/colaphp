c09acb5cfbfdc62932b7190e730c67114c16a771
wanganlin <inbox@wanganlin.com>
2022-03-21 13:09:37 +0800

    更新控制器

---
+++ app/Http/Controllers/Auth/ForgotController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Swift\Http\Request;
+use Swift\Http\Response;
+
+/**
+ * Class ForgotController
+ * @package App\Http\Controllers\Auth
+ */
+class ForgotController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function forgot(Request $request): Response
+    {
+        return $this->succeed('auth/forgot');
+    }
+}
---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Services\Auth\Internal\Input\LoginInput;
+use App\Services\Auth\Internal\Input\MobileLoginInput;
+use App\Services\Auth\PassportService;
+use Exception;
+use Respect\Validation\Exceptions\ValidationException;
+use Respect\Validation\Validator;
+use Swift\Http\Request;
+use Swift\Http\Response;
+use Swift\Log\Log;
+
+/**
+ * Class LoginController
+ * @package App\Http\Controllers\Auth
+ */
+class LoginController extends Controller
+{
+    /**
+     * 用户名、邮箱、手机号码与密码登录
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        try {
+            $data = Validator::input($request->post(), [
+                'passport' => Validator::notBlank()->setName('账号'),
+                'password' => Validator::length(6, 64)->setName('密码'),
+                'captcha' => Validator::length(4, 6)->setName('验证码')
+            ]);
+        } catch (ValidationException $e) {
+            return $this->failed($e->getMessage());
+        }
+
+        if ($request->session()->get('CAPTCHA') !== $data['captcha']) {
+            return $this->failed('图片验证码不正确');
+        }
+
+        $loginInput = new LoginInput();
+        $loginInput->setUsername($request->post('username'));
+        $loginInput->setPassword($request->post('password'));
+
+        try {
+            $passportService = new PassportService();
+            $userId = $passportService->login($loginInput);
+            $token = $passportService->createToken($userId);
+            return $this->succeed($token)->cookie(USER_TOKEN, $token, 0, '/', '', false, true);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return $this->failed('用户名或密码不正确');
+        }
+    }
+
+    /**
+     * 手机号码与短信验证码登录
+     * @param Request $request
+     * @return Response
+     */
+    public function mobile(Request $request): Response
+    {
+        try {
+            $data = Validator::input($request->post(), [
+                'mobile' => Validator::regex(MOBILE_REGEX)->setName('手机号码'),
+                'sms_code' => Validator::length(4, 6)->setName('短信验证码')
+            ]);
+        } catch (ValidationException $e) {
+            return $this->failed($e->getMessage());
+        }
+
+        if ($request->session()->get(USER_SMS_CODE . $data['mobile']) !== $data['sms_code']) {
+            return $this->failed('短信验证码不正确');
+        }
+
+        $mobileLoginInput = new MobileLoginInput();
+        $mobileLoginInput->setMobile($request->post('mobile'));
+        $mobileLoginInput->setSmsCode($request->post('sms_code'));
+
+        try {
+            $passportService = new PassportService();
+            $userId = $passportService->loginByMobile($mobileLoginInput);
+            $token = $passportService->createToken($userId);
+            return $this->succeed($token)->cookie(USER_TOKEN, $token, 0, '/', '', false, true);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return $this->failed('手机验证码不正确');
+        }
+    }
+
+    /**
+     * 社会化授权登录
+     * @param Request $request
+     * @return void
+     */
+    public function socialize(Request $request)
+    {
+        // todo
+    }
+}
