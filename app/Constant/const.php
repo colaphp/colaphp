4c0c07b6e6eb44a757138de367ec137d94e4f65e
wanganlin <inbox@wanganlin.com>
2022-03-21 13:06:07 +0800

    增加常量定义

--- app/Constant/const.php
+++ app/Constant/const.php
@@ -1,1 +1,12 @@
 <?php
+
+// 用户验证码
+const USER_CAPTCHA = 'USER_CAPTCHA';
+// 用户短信码
+const USER_SMS_CODE = 'USER_SMS_CODE';
+// 用户认证
+const USER_TOKEN = 'USER_TOKEN';
+// 用户凭证
+const USER_AUTH = 'USER_AUTH';
+// 手机号正则
+const MOBILE_REGEX = '/^1[3-9][0-9]\d{8}$/';
