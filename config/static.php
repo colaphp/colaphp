00b845d90ded8ead5a093563e69820d7a10d76b9
wanganlin <inbox@wanganlin.com>
2022-03-21 13:22:13 +0800

    optimize

--- config/static.php
+++ config/static.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'enable' => false,
-    'middleware' => [ // Static file Middleware
-        // \App\Http\Middleware\StaticFile::class,
+    'enable' => true,
+    'middleware' => [
+        App\Http\Middleware\StaticFile::class,
     ],
 ];
