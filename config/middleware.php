6ced17054feca6fda3d647b9e906783146f0c937
wanganlin <inbox@wanganlin.com>
2022-03-25 09:25:41 +0800

    更新跨域请求中间件

--- config/middleware.php
+++ config/middleware.php
@@ -3,19 +3,18 @@
 return [
     '' => [
         // Global middleware
+        Swift\Http\Middleware\HandleCors::class,
     ],
     'Auth' => [
         App\Http\Middleware\RedirectIfAuthenticated::class,
     ],
     'Shop' => [
-        App\Http\Middleware\HandleCors::class,
     ],
     'Seller' => [
         App\Http\Middleware\Authenticate::class,
         App\Http\Middleware\Authorization::class,
     ],
     'User' => [
-        App\Http\Middleware\HandleCors::class,
         App\Http\Middleware\Authenticate::class,
     ],
 ];
