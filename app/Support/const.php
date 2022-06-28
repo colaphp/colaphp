bbc7f2fefcbf187b67bce9300cfe01a317854524
wanganlin <inbox@wanganlin.com>
2022-06-28 17:44:17 +0800

    update

--- app/Support/const.php
+++ app/Support/const.php
@@ -8,5 +8,3 @@ const USER_SMS_CODE = 'USER_SMS_CODE';
 const USER_TOKEN = 'USER_TOKEN';
 // 用户凭证
 const USER_AUTH = 'USER_AUTH';
-// 手机号正则
-const MOBILE_REGEX = '/^1[3-9][0-9]\d{8}$/';
--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,13 +1,23 @@
 <?php
 
-function csrf_token(): string
-{
-    return md5('a');
-}
+// 手机号正则
+const MOBILE_REGEX = '/^1[3-9][0-9]\d{8}$/';
 
+/**
+ * @param string $url
+ * @return string
+ */
 function asset(string $url): string
 {
     return '/' . $url . '?v=' . RELEASE;
+}
+
+/**
+ * @return string
+ */
+function csrf_token(): string
+{
+    return request()->sessionId();
 }
 
 /**
@@ -18,4 +28,13 @@ function asset(string $url): string
 function skin(string $url): string
 {
     return asset('themes/default/' . ltrim($url, '/'));
+}
+
+/**
+ * @param string $url
+ * @return string
+ */
+function url(string $url): string
+{
+    return $url;
 }
