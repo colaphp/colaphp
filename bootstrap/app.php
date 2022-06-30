b7e930ce8ffb68b1537355817be61c48a235b3e6
wanganlin <inbox@wanganlin.com>
2022-06-30 09:56:44 +0800

    version

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
-if (version_compare(PHP_VERSION, '8.1', '<')) {
-    die('Require a PHP version ">=8.1". You are running ' . PHP_VERSION . '.');
+if (version_compare(PHP_VERSION, '8.0.2', '<')) {
+    die('Require a PHP version ">= 8.0.2". You are running ' . PHP_VERSION . '.');
 }
 
 const APP_NAME = 'PHPMall';
-const VERSION = 'v1.0.8';
-const RELEASE = '20220321';
+const VERSION = 'v1.1.0';
+const RELEASE = '20220630';
