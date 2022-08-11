e57bce7f5f450b9fca218029f26122ac3e29be38
wanganlin <inbox@wanganlin.com>
2022-08-11 09:55:28 +0800

    增加验证码服务

---
+++ app/Services/Captcha/CaptchaService.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Captcha;
+
+use Gregwar\Captcha\CaptchaBuilder;
+use Gregwar\Captcha\PhraseBuilder;
+
+class CaptchaService
+{
+    const SESSION_KEY = 'CAPTCHA';
+
+    /**
+     * 生成图片验证码
+     *
+     * @param  int  $length
+     * @param  string  $suffix
+     * @return bool|string
+     */
+    public function create(int $length = 4, string $suffix = ''): bool|string
+    {
+        $phraseBuilder = new PhraseBuilder($length, '023456789');
+        // 初始化验证码
+        $builder = new CaptchaBuilder(null, $phraseBuilder);
+        // 生成验证码
+        $builder->build(120);
+        // 将验证码的值存储
+        session([CaptchaService::SESSION_KEY.$suffix => $builder->getPhrase()]);
+        // 获得验证码图片二进制数据
+        return $builder->get();
+    }
+
+    /**
+     * 验证图片验证码
+     *
+     * @param  string  $captcha
+     * @param  string  $suffix
+     * @return bool
+     */
+    public function valid(string $captcha = '', string $suffix = ''): bool
+    {
+        return session(CaptchaService::SESSION_KEY.$suffix) === $captcha;
+    }
+}
