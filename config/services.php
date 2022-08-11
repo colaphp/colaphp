cc3d23a6dae3b320f57d1845caa9fbeec8b45645
wanganlin <inbox@wanganlin.com>
2022-08-11 09:48:00 +0800

    增加oauth配置

---
+++ config/services.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'qq' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://example.com/callback-url',
+    ],
+    'wechat' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://example.com/callback-url',
+    ],
+    'weibo' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://example.com/callback-url',
+    ],
+];
