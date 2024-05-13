b7b1566a082ae9a095a7d728752901912eaf9abc
wanganlin <inbox@wanganlin.com>
2024-05-13 11:34:46 +0800

    异常前端项目

--- desktop/src/pages/admin/index.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <main>
-    The admin page
-  </main>
-</template>
--- desktop/src/pages/user/profile.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <main>
-    The user profile
-  </main>
-</template>
