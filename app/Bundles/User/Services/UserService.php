e0a6b639a73d66801f6d87f838fdfa050523d472
wanganlin <inbox@wanganlin.com>
2024-05-13 11:03:44 +0800

    更新脚手架

--- app/Bundles/User/Services/UserService.php
+++ app/Bundles/User/Services/UserService.php
@@ -2,31 +2,8 @@
 
 declare(strict_types=1);
 
-namespace App\Service\User;
+namespace App\Bundles\User\Services;
 
-use App\Models\User;
-use App\Service\User\Output\UserOutput;
-
-/**
- * Class UserService
- */
 class UserService
 {
-    /**
-     * @param  int  $userId
-     * @return UserOutput|null
-     */
-    public function findOne(int $userId): ?UserOutput
-    {
-        $user = User::query()->first($userId);
-
-        $userOutput = new UserOutput();
-        if ($user) {
-            $userOutput->setId($user->id);
-
-            return $userOutput;
-        }
-
-        return null;
-    }
 }
